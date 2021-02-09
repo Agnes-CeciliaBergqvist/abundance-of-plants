5ccaf811dcb80e1a8f37f1ee1210cf8af4e5ca63
Agnes-CeciliaBergqvist <agnes.bergqvist@hotmail.com>
2021-02-09 13:36:54 +0100

    created function for clear and calculating function

---
+++ js.js
@@ -0,0 +1,29 @@
+//Program should calculate dropdown number with input field number
+var list = [];
+
+
+const enterNumber = document.getElementById('enter-number')
+const dropDownMeny = document.getElementById('drop-down')
+const addBtn = document.getElementById('addBtn')
+const Result = document.getElementById('Result')
+
+
+addBtn.onclick = () => {
+    var result = enterNumber.valueAsNumber * Number(dropDownMeny.value);
+    var li = document.createElement("li");
+    li.innerText = result;
+
+    var ul = document.querySelector(".ulList");
+    ul.appendChild(li);
+}
+
+
+//Clearing function 
+function clear() {
+    location.reload()
+}
+
+var clearButton = document.querySelector("#clear")
+console.log(clearButton)
+
+clearButton.addEventListener("click", clear);
